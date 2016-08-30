df1e8ed7ee29fe656c04e81372b5fde93da99816
ogirginc <ogirginc@gmail.com>
2016-08-30 18:38:03 +0300

    Add viewing list

--- spec/features/listview_spec.rb
+++ spec/features/listview_spec.rb
@@ -3,13 +3,9 @@ require 'rails_helper'
 feature 'Listview' do
   context 'when instructions are added' do
     scenario 'displays a list of available instructions' do
-      # instructions1 = double('instructions')
-      # instructions2 = double('instructions')
       visit root_path
       expect(current_path).to eq '/'
       expect(page).to have_selector('ul')
-      # expect(list).not_to be_empty
     end
   end
 end
-# visit root_path
