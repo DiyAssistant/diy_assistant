5d5c41f209b88b27a09e9819215d6c9465c3fa49
6eff <6eff@mail.ru>
2016-08-30 17:14:03 +0300

    adds index.html.erb

--- spec/features/listview_spec.rb
+++ spec/features/listview_spec.rb
@@ -5,9 +5,11 @@ feature 'Listview' do
     scenario 'displays a list of available instructions' do
       # instructions1 = double('instructions')
       # instructions2 = double('instructions')
-
+      visit root_path
       expect(current_path).to eq '/'
-      expect(list).not_to be_empty
+      expect(page).to have_selector('ul')
+      # expect(list).not_to be_empty
     end
   end
 end
+# visit root_path
