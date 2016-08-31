feea3696b9faabf8e4dcaa8aac589861b40131bf
prashantmathias <mathiasaudio@gmail.com>
2016-08-31 16:40:21 +0100

    adds test for image upload

--- spec/features/instructions_features_spec.rb
+++ spec/features/instructions_features_spec.rb
@@ -9,4 +9,16 @@ feature 'instructions' do
     expect(page).to have_content 'making my own desk'
     expect(current_path).to eq '/instructions/1'
   end
+
+  scenario 'is able to upload a pic with the instructions post' do
+    sign_up
+    click_link 'Create guide'
+    fill_in 'Title', with: 'How to build a desk'
+    fill_in 'Description', with: 'making my own desk'
+    page.attach_file('instruction_image', Rails.root + 'spec/features/test_image/BK_Logo.jpg')
+    click_button 'Publish'
+
+    expect(page).to have_css("img")
+    expect(current_path).to eq '/instructions/2'
+  end
 end
