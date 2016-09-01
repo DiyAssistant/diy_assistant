739c503b760561062612354452219d7296122bdd
ogirginc <ogirginc@gmail.com>
2016-09-02 01:10:56 +0300

    Add Coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -7,6 +8,8 @@ require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
 require 'support/database_cleaner'
+require 'coveralls'
+Coveralls.wear!('rails')
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
