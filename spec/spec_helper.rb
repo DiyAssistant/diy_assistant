7a285a3ad6c2e29ca86d743c63c5fa702d1d1d49
ogirginc <ogirginc@gmail.com>
2016-09-02 01:49:34 +0300

    Add Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@
 #
 require 'rails_helper'
 require_relative 'helpers/web_helper'
-require 'coveralls'
-Coveralls.wear!
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
 # will add to the boot time of your test suite on EVERY test run, even for an
